7501374d1f656bc3744d61565dba1213a90b25fc
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2025-03-21 23:18:44 -0500

    Define Boton component attributes and function

--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -1,9 +1,20 @@
 function Boton(props) {
+
+    const isOperator = value => {
+      if(isNaN(value) && value != '.' && value != '='){
+        return true;
+      } else {
+        return false;
+      }
+    };
+
     return (
-        <div>
+        <div
+          className={`button-contenedor ${isOperator(props.children) ? 'operator' : null}`}>
+         {/* Permite que el componente acepte múltiples hijos */}
          {props.children} 
         </div>
     );
 }
-
+  
 export default Boton;
